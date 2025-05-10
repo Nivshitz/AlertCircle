bba2d63bc894032bf08562589e19cf718153643f
Nivshitz <nivshir92@gmail.com>
2025-05-10 18:16:32 +0000

    updated req.txt, rewrote process_alerts_stream.py

--- user_report_simulator.py
+++ user_report_simulator.py
@@ -16,21 +16,17 @@ def generate_event(report_options):
         "event_type": random.choice(report_options),
         "user_id": f"user_{random.randint(1000, 9999)}",
         "description": "Saw an insepctor giving reports to dog owners", # Event description provided by the user (optionally)
-        "location": {
-            "latitude": round(random.uniform(-90.0, 90.0), 6),
-            "longitude": round(random.uniform(-180.0, 180.0), 6),
-            #"accuracy": round(random.uniform(3.0, 50.0), 1)  # meters
-        },
+        "latitude": round(random.uniform(-90.0, 90.0), 6),
+        "longitude": round(random.uniform(-180.0, 180.0), 6),
+        #"accuracy": round(random.uniform(3.0, 50.0), 1)  # meters
         "device_id": f"device_{random.randint(1000, 9999)}"
-        #"app_version": f"{random.randint(1, 3)}.{random.randint(0, 9)}.{random.randint(0, 9)}",
-        #"photo_url": None  # Placeholder, can be extended later
     }
 
 try:
     while True:
         event = json.dumps(generate_event(report_options))
         print(event)
-        producer.send(topic = 'raw_alerts', value = event)
+        producer.send(topic='raw_alerts', value=event)
 
         time.sleep(10)
 
