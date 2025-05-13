07c4a62cdf054b55aea9fcfa1c3b1df746fb3abb
Nivshiz <Nivshir92@gmail.com>
2025-05-13 17:06:58 +0300

    updated update_user_locations_dag.py so it include stdout catch in airflow to see logs and error

--- update_user_locations_dag.py
+++ update_user_locations_dag.py
@@ -3,22 +3,23 @@ from airflow.providers.ssh.operators.ssh import SSHOperator
 from datetime import datetime, timedelta
 
 default_args = {
-    'owner': 'nivshitz',
+    'owner': 'airflow',
+    'start_date': datetime(2025, 5, 9),
     'retries': 1,
     'retry_delay': timedelta(minutes=1)
 }
 
 with DAG(
-    dag_id='update_user_locations_ssh_dag',
+    dag_id='update_user_locations_dag',
     default_args=default_args,
     description='Update MongoDB using SSHOperator',
-    schedule_interval='*/1 * * * *',
-    start_date=datetime(2025, 5, 9),
-    catchup=False,
-    max_active_runs=1
+    schedule_interval='* * * * *',  # runs every minute
+    catchup=False
 ) as dag:
+
     update_task = SSHOperator(
         task_id='update_user_locations',
         ssh_conn_id='dev_env_ssh',
-        command='python3 /home/developer/airflow/update_user_locations.py',
+        command='python3 /home/developer/AlertCircle/update_user_locations.py',
+        do_xcom_push=True  # captures stdout into logs
     )
