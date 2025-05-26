c6c07b5cb36ab2a13ccbdead115965b1ebb67abb
Nivshiz <Nivshir92@gmail.com>
2025-05-26 19:44:12 +0300

    Updated user report simulator for it's final version

--- user_report_simulator.py
+++ user_report_simulator.py
@@ -1,37 +1,67 @@
 import random
 import json
-from datetime import datetime, timezone
 import uuid
 import time
+from datetime import datetime, timezone
 from kafka import KafkaProducer
+import os
+from dotenv import load_dotenv
 
-producer = KafkaProducer(bootstrap_servers = 'course-kafka:9092',
-                         value_serializer = lambda v: v.encode('utf-8'))
+# Load environment variables
+load_dotenv()
+EC2_EXTERNAL_IP = os.getenv("EC2_EXTERNAL_IP")
 
-report_options = ['inspector_dog', 'inspector_parking']
+# Config
+RAW_TOPIC = "raw_alerts"
+KAFKA_BOOTSTRAP_SERVERS = f"{EC2_EXTERNAL_IP}:9092"
 
-def generate_event(report_options):
+# Kafka producer setup
+producer = KafkaProducer(
+    bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
+    value_serializer=lambda v: v.encode('utf-8')
+)
+
+# Known users
+known_users = [
+    "niv123", "charlie789", "tamar456", "shira321", "amir888",
+    "lior111", "gal654", "eden990", "yossi555", "noa007"
+]
+
+# Descriptions
+descriptions = [
+    "Inspector just fined a woman with a dog",
+    "Dog inspector writing tickets again...",
+    "Bylaw officer arguing with a guy at the park",
+    "Saw an inspector near the dog area",
+    "Someone's getting a ticket for no leash"
+]
+
+# Location bounds: Tel Aviv – Ramat Gan – Givatayim
+lat_range = (32.04, 32.10)
+lon_range = (34.78, 34.83)
+
+# Event generator
+def generate_event():
     return {
         "event_id": str(uuid.uuid4()),
         "event_time": datetime.now(timezone.utc).isoformat(),
-        "event_type": random.choice(report_options),
-        "user_id": f"user_{random.randint(1000, 9999)}",
-        "description": "Saw an insepctor giving reports to dog owners", # Event description provided by the user (optionally)
-        "latitude": round(random.uniform(-90.0, 90.0), 6),
-        "longitude": round(random.uniform(-180.0, 180.0), 6),
-        "device_id": f"device_{random.randint(1000, 9999)}"
+        "event_type": "bylaw_dog_inspector",
+        "user_id": random.choice(known_users),
+        "description": random.choice(descriptions),
+        "latitude": round(random.uniform(*lat_range), 6),
+        "longitude": round(random.uniform(*lon_range), 6)
     }
 
+# Producer loop
 try:
     while True:
-        event = json.dumps(generate_event(report_options))
-        print(event)
-        producer.send(topic='raw_alerts', value=event)
-
-        time.sleep(10)
+        event = json.dumps(generate_event())
+        print(f"Sending: {event}")
+        producer.send(RAW_TOPIC, value=event)
+        time.sleep(20)
 
 except Exception as e:
-    print(f"An error occurred: {e}")
+    print(f"Error occurred: {e}")
 
 finally:
     producer.flush()
